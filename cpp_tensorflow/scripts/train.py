fcebf57121b49976e022fcdb301acbeb454167f6
p-geon <nagano@nlab.ci.i.u-tokyo.ac.jp>
2021-02-20 11:16:19 +0900

    mod

---
+++ cpp_tensorflow/scripts/train.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+print(tf.version.VERSION)
+
+class Model:
+    def __init__(self):
+        pass
+
+def main():
+    pass
+
+if(__name__=="__main__"):
+    main
